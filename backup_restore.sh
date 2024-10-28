e3f2ae06220e06f495a432164cca34e45e16059a
RichardLeite <rs9010482@gmail.com>
2024-10-28 17:28:50 -0300

    feat: add new config option to define backup and restore directory

--- backup_restore.sh
+++ backup_restore.sh
@@ -2,15 +2,17 @@
 
 CONFIG_FILE=".conf"
 
-configure_backup_dir() {
+configure_dirs() {
     echo
-    echo "Configuração do Diretório de Backup"
-    echo "=================================="
+    echo "Configuração dos Diretórios"
+    echo "=========================="
+    
+    echo "Configuração do Diretório de Backup:"
     echo "1) Padrão (pasta atual)"
     echo "2) Personalizado"
-    read -p "Escolha uma opção (1-2): " opcao
+    read -p "Escolha uma opção (1-2): " backup_opcao
 
-    case $opcao in
+    case $backup_opcao in
         1)
             backup_path="$(pwd)"
             ;;
@@ -22,68 +24,80 @@ configure_backup_dir() {
             backup_path="$(pwd)"
             ;;
     esac
-    
+
+    echo
+    echo "Configuração do Diretório de Restauração:"
+    echo "1) Padrão (pasta atual)"
+    echo "2) Personalizado"
+    read -p "Escolha uma opção (1-2): " restore_opcao
+
+    case $restore_opcao in
+        1)
+            restore_path="$(pwd)"
+            ;;
+        2)
+            read -p "Digite o caminho completo onde está o arquivo backup.tar.xz: " restore_path
+            ;;
+        *)
+            echo "Opção inválida. Usando diretório padrão."
+            restore_path="$(pwd)"
+            ;;
+    esac
+
     mkdir -p "$backup_path/backup"
-    echo "BACKUP_DIR=$backup_path/backup" > "$CONFIG_FILE"
     
-    echo "Configuração salva com sucesso!"
-    echo "Diretório configurado: $backup_path/backup"
+    echo "BACKUP_DIR=$backup_path/backup" > "$CONFIG_FILE"
+    echo "RESTORE_DIR=$restore_path" >> "$CONFIG_FILE"
+    
+    echo
+    echo "Configurações salvas com sucesso!"
+    echo "Diretório de backup: $backup_path/backup"
+    echo "Diretório de restauração: $restore_path"
     echo
 }
-
 
 load_config() {
     if [ -f "$CONFIG_FILE" ]; then
         source "$CONFIG_FILE"
     else
         echo "Configuração não encontrada!"
-        configure_backup_dir
+        configure_dirs
     fi
 }
 
-# Função de Backup
 backup() {
     echo
     echo "Iniciando backup..."
     echo "==================="
 
-    echo "$BACKUP_DIR"
-
     TEMP_DIR="$BACKUP_DIR/temp"
 
-    # Cria o diretório de backup e o temporário, se não existirem
     mkdir -p "$BACKUP_DIR"
     mkdir -p "$TEMP_DIR"
 
-    # Backup dos dotfiles
     echo "Compactando dotfiles..."
     mkdir -p "$TEMP_DIR/dotfiles"
     cp ~/.zshrc ~/.bashrc "$TEMP_DIR/dotfiles"
     tar -cJf "$BACKUP_DIR/dotfiles.tar.xz" -C "$TEMP_DIR" dotfiles
     rm -rf "$TEMP_DIR/dotfiles"
 
-    # Backup do diretório .config
     echo "Compactando .config..."
     cp -r "$HOME/.config" "$TEMP_DIR"
     tar -cJf "$BACKUP_DIR/config.tar.xz" -C "$TEMP_DIR" .config
     rm -rf "$TEMP_DIR/.config"
 
-    # Backup do Powerlevel10k
     echo "Compactando tema Powerlevel10k..."
     cp -r "$HOME/.oh-my-zsh/custom/themes/powerlevel10k" "$TEMP_DIR"
     tar -cJf "$BACKUP_DIR/powerlevel10k.tar.xz" -C "$TEMP_DIR" powerlevel10k
     rm -rf "$TEMP_DIR/powerlevel10k"
 
-    # Backup da lista de pacotes instalados
     echo "Salvando lista de pacotes APT..."
     dpkg --get-selections > "$BACKUP_DIR/packages-list.txt"
 
-    # Lista de pacotes Snap e Flatpak
     echo "Salvando lista de pacotes Snap e Flatpak..."
     snap list > "$BACKUP_DIR/snap-list.txt"
     flatpak list > "$BACKUP_DIR/flatpak-list.txt"
 
-    # Compactação dos temas, fontes e extensões GNOME
     echo "Compactando temas, fontes e extensões GNOME..."
     cp -r /usr/share/themes "$TEMP_DIR"
     tar -cJf "$BACKUP_DIR/themes.tar.xz" -C "$TEMP_DIR" themes
@@ -105,56 +119,49 @@ backup() {
     tar -cJf "$BACKUP_DIR/gnome-extensions-system.tar.xz" -C "$TEMP_DIR" gnome-extensions-system
     rm -rf "$TEMP_DIR/gnome-extensions-system"
 
-    # Backup das configurações do GNOME Extensions no dconf
     echo "Salvando configurações do GNOME Extensions..."
     dconf dump /org/gnome/shell/extensions/ > "$BACKUP_DIR/gnome-extensions-settings.dconf"
 
-    # Criar arquivo único de backup
     tar -cJf backup.tar.xz -C "$BACKUP_DIR" .
 
-    # Removendo diretório temporário após o backup
     rm -rf "$TEMP_DIR"
     rm -rf "$BACKUP_DIR"
     
     echo "Backup concluído! Arquivos salvos e compactados em backup.tar.xz"
 }
 
-# Função de Restauração
 restore() {
     echo
     echo "Iniciando restauração..."
     echo "========================"
 
-    # Criar diretório temporário
+    if [ ! -f "$RESTORE_DIR/backup.tar.xz" ]; then
+        echo "Arquivo backup.tar.xz não encontrado em: $RESTORE_DIR"
+        return 1
+    fi
+
     TEMP_RESTORE_DIR="./temp_restore"
     mkdir -p "$TEMP_RESTORE_DIR"
     
-    # Extrair backup principal
-    tar -xJf ./backup.tar.xz -C "$TEMP_RESTORE_DIR"
+    tar -xJf "$RESTORE_DIR/backup.tar.xz" -C "$TEMP_RESTORE_DIR"
     
-    # Continuar com a restauração usando os arquivos do diretório temporário
     BACKUP_DIR="$TEMP_RESTORE_DIR"
 
-    # Restauração dos dotfiles
     echo "Restaurando dotfiles..."
     tar -xJf "$BACKUP_DIR/dotfiles.tar.xz" -C "$HOME"
 
-    # Restauração do diretório .config
     echo "Restaurando .config..."
     tar -xJf "$BACKUP_DIR/config.tar.xz" -C "$HOME"
 
-    # Restauração do Powerlevel10k
     echo "Restaurando tema Powerlevel10k..."
     mkdir -p ~/.oh-my-zsh/custom/themes
     tar -xJf "$BACKUP_DIR/powerlevel10k.tar.xz" -C ~/.oh-my-zsh/custom/themes
 
-    # Restauração da lista de pacotes APT
     if [[ -f "$BACKUP_DIR/packages-list.txt" ]]; then
         echo "Restaurando pacotes APT..."
         sudo dpkg --set-selections < "$BACKUP_DIR/packages-list.txt" && sudo apt-get dselect-upgrade
     fi
 
-    # Restauração de pacotes Snap e Flatpak
     if [[ -f "$BACKUP_DIR/snap-list.txt" ]]; then
         echo "Restaurando pacotes Snap..."
         xargs -a "$BACKUP_DIR/snap-list.txt" -I {} sudo snap install {}
@@ -164,7 +171,6 @@ restore() {
         xargs -a "$BACKUP_DIR/flatpak-list.txt" -I {} flatpak install flathub {}
     fi
 
-    # Restauração de temas, fontes e extensões GNOME
     echo "Restaurando temas, fontes e extensões GNOME..."
     sudo tar -xJf "$BACKUP_DIR/themes.tar.xz" -C /usr/share
     sudo tar -xJf "$BACKUP_DIR/icons.tar.xz" -C /usr/share
@@ -172,27 +178,22 @@ restore() {
     tar -xJf "$BACKUP_DIR/gnome-extensions-user.tar.xz" -C "$HOME/.local/share/gnome-shell"
     sudo tar -xJf "$BACKUP_DIR/gnome-extensions-system.tar.xz" -C /usr/share/gnome-shell
 
-    # Restauração das configurações do GNOME Extensions no dconf
     echo "Restaurando configurações do GNOME Extensions..."
     dconf load /org/gnome/shell/extensions/ < "$BACKUP_DIR/gnome-extensions-settings.dconf"
 
-    # Limpar diretório temporário ao finalizar
     rm -rf "$TEMP_RESTORE_DIR"
     
     echo "Restauração concluída!"
 }
 
-# Menu de seleção
 main() {
-    # Verificação inicial do arquivo de configuração
     if [ ! -f "$CONFIG_FILE" ]; then
         echo
         echo "Primeira execução detectada - Configuração inicial necessária"
         echo "========================================================"
-        configure_backup_dir
+        configure_dirs
     fi
 
-    # Menu principal
     while true; do
         echo
         echo "Bem-vindo ao Zorin OS Backup & Restore"
@@ -206,7 +207,7 @@ main() {
         do
             case $opt in
                 "Configuração")
-                    configure_backup_dir
+                    configure_dirs
                     break
                     ;;
                 "Backup")
@@ -231,5 +232,4 @@ main() {
     done
 }
 
-# Iniciar o programa
 main
